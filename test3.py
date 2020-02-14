fed35a40e2e5bd875c157e9f1666af0be526bb5e
PinyaevKirill <pinyaev-kirill@ya.ru>
2020-02-14 17:11:00 +0300

    задача 4

--- test3.py
+++ test3.py
@@ -7,17 +7,31 @@ import requests
 coord_x = '37.490971'
 coord_y = '55.829152'
 delta = '0.02'
+tip = "map"
+
+
+def on_click(cell_coords):
+    global delta, coord_x, coord_y, tip
+    if cell_coords[0] > 600 and cell_coords[1] < 40:
+        tip = "map"
+        map = request()
+    if cell_coords[0] > 600 and 40 < cell_coords[1] < 80:
+        tip = "sat"
+        map = request()
+    if cell_coords[0] > 600 and 80 < cell_coords[1] < 120:
+        tip = "sat,skl"
+        map = request()
 
 
 def request():
-    global delta, coord_x, coord_y
+    global delta, coord_x, coord_y, tip
     response = None
     map_request = "http://static-maps.yandex.ru/1.x/?"
 
     map_params = {
         "ll": ",".join([coord_x, coord_y]),
         "spn": ",".join([delta, delta]),
-        "l": "map"
+        "l": tip
     }
 
     response = requests.get(map_request, params=map_params)
@@ -37,7 +51,25 @@ def request():
 map = request()
 
 pygame.init()
-screen = pygame.display.set_mode((600, 450))
+screen = pygame.display.set_mode((720, 450))
+screen.fill((255, 255, 255))
+font = pygame.font.Font(None, 40)
+text = font.render("Схема", 1, (0, 0, 0))
+text_x = 600
+text_y = 0
+screen.blit(text, (text_x, text_y))
+font = pygame.font.Font(None, 40)
+text = font.render("Спутник", 1, (0, 0, 0))
+text_x = 600
+text_y = 40
+screen.blit(text, (text_x, text_y))
+font = pygame.font.Font(None, 40)
+text = font.render("Гибрид", 1, (0, 0, 0))
+text_x = 600
+text_y = 80
+screen.blit(text, (text_x, text_y))
+
+
 
 
 running = True
@@ -46,7 +78,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-
             if event.key == 280:
                 if float(delta) + 2 < 17:
                     delta = str(float(delta) + 2)
@@ -62,21 +93,33 @@ while running:
                     coord_y = str(float(coord_y) + 1)
                     print(coord_y)
                     map = request()
-            elif event.key == pygame.K_DOWN:
+            if event.key == pygame.K_DOWN:
                 if float(coord_y) - 1 > -80:
                     coord_y = str(float(coord_y) - 1)
                     print(coord_y)
                     map = request()
-            elif event.key == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT:
                 if float(coord_x) - 1 > -180:
                     coord_x = str(float(coord_x) - 1)
                     print(coord_x)
                     map = request()
-            elif event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_RIGHT:
                 if float(coord_x) + 1 < 180:
                     coord_x = str(float(coord_x) + 1)
                     print(coord_x)
                     map = request()
+            if event.key == pygame.K_1:
+                tip = "map"
+                map = request()
+            if event.key == pygame.K_2:
+                tip = "sat"
+                map = request()
+            if event.key == pygame.K_3:
+                tip = "sat,skl"
+                map = request()
+        if event.type == pygame.MOUSEBUTTONUP:
+            on_click(event.pos)
+
     pygame.display.flip()
     screen.blit(pygame.image.load(map), (0, 0))
 
