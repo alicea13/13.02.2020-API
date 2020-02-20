45050821ac8543c64cca5d1339eaef8bf45cc783
alicea13 <alisa-ays@mail.ru>
2020-02-20 13:23:27 +0300

    задача 5 полностью

--- test5.py
+++ test5.py
@@ -5,22 +5,29 @@ import pygame
 import requests
 
 
+coord_x = '37.490971'
+coord_y = '55.829152'
+delta = '0.02'
+tip = "map"
+
+
 class InputWindow:
-    def __init__(self, x, y, width, height, delta, tip, screen, par):
+    def __init__(self, screen, par):
+        global coord_x, coord_y, delta, tip
 
         self.text = ''
         self.screen = screen
-        self.rect = pygame.Rect(x, y, width, height)
+        self.rect = pygame.Rect(0, 0, 600 + 2, 40)
         self.text_surf = pygame.font.Font(None, 32).render(self.text, True, (0, 0, 0))
         # self.run_input = False
         self.parent = par
 
         self.color = "black"
 
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+        self.x = 0
+        self.y = 0
+        self.width = 600
+        self.height = 40
 
         self.delta = delta
         self.tip = tip
@@ -38,8 +45,9 @@ class InputWindow:
 
         if event.type == pygame.KEYDOWN and self.run_text:
             if event.key == pygame.K_RETURN:
-                self.parent.coord = self.search(self.text)
                 request(self.search(self.text), self.delta, self.tip)
+                self.parent.coord_x, self.parent.coord_y = self.search(self.text)
+                self.parent.coord = [self.parent.coord_x, self.parent.coord_y]
                 self.text = ''
             elif event.key == pygame.K_BACKSPACE:
                 self.text = self.text[:-1]
@@ -50,6 +58,7 @@ class InputWindow:
         return self.parent.coord
 
     def draw(self, screen):
+        pygame.draw.rect(screen, pygame.Color("white"), self.rect, 0)
         screen.blit(self.text_surf, (self.rect.x + 5, self.rect.y + 5))
 
         pygame.draw.rect(screen, pygame.Color(self.color), self.rect, 3)
@@ -77,7 +86,8 @@ class InputWindow:
 
 
 class Window:
-    def __init__(self, coord_x, coord_y, delta, tip):
+    def __init__(self):
+        global coord_x, coord_y, delta, tip
 
         pygame.init()
         screen = pygame.display.set_mode((720, 490))
@@ -90,43 +100,44 @@ class Window:
         self.delta = delta
         self.tip = tip
 
-        text = font.render("Схема", 1, (0, 0, 0))
-        text_x = 620
-        text_y = 50
-        text_w = text.get_width()
-        text_h = text.get_height()
+        text_map = font.render("Схема", 1, (0, 0, 0))
+        text_x_map = 620
+        text_y_map = 50
+        text_w_map = text_map.get_width()
+        text_h_map = text_map.get_height()
 
-        pygame.draw.rect(screen, (0, 0, 0), (600, text_y - 10,
-                                             630, text_h + 20), 2)
+        pygame.draw.rect(screen, (0, 0, 0), (600, text_y_map - 10,
+                                             630, text_h_map + 20), 2)
+        self.color_gibr = (0, 0, 0)
 
-        screen.blit(text, (text_x, text_y))
         font = pygame.font.Font(None, 40)
-        text = font.render("Спутник", 1, (0, 0, 0))
-        text_x = 605
-        text_y = 98
-        text_w = text.get_width()
-        text_h = text.get_height()
-        pygame.draw.rect(screen, (0, 0, 0), (600, text_y - 10,
-                                             text_w + 20, text_h + 20), 2)
-        screen.blit(text, (text_x, text_y))
-        font = pygame.font.Font(None, 40)
-        text = font.render("Гибрид", 1, (0, 0, 0))
-        text_x = 610
-        text_y = 146
-        screen.blit(text, (text_x, text_y))
-        text_w = text.get_width()
-        text_h = text.get_height()
-        pygame.draw.rect(screen, (0, 0, 0), (600, text_y - 10,
-                                             text_w + 20, text_h + 20), 2)
+        text_spn = font.render("Спутник", 1, (0, 0, 0))
+        text_x_spn = 605
+        text_y_spn = 98
+        text_w_spn = text_spn.get_width()
+        text_h_spn = text_spn.get_height()
 
-        '''pygame.draw.rect(screen, (0, 0, 0), (600, 0, 120, 40), 2)
+        pygame.draw.rect(screen, (0, 0, 0), (600, text_y_spn - 10,
+                                             text_w_spn + 20, text_h_spn + 20), 2)
+        self.color_spn = (0, 0, 0)
+
         font = pygame.font.Font(None, 40)
-        text = font.render("Искать", 1, (0, 0, 0))
-        screen.blit(text, (612, 8))'''
+        text_gibr = font.render("Гибрид", 1, (0, 0, 0))
+        text_x_gibr = 610
+        text_y_gibr = 146
+        text_w_gibr = text_gibr.get_width()
+        text_h_gibr = text_gibr.get_height()
+        pygame.draw.rect(screen, (0, 0, 0), (600, text_y_gibr - 10,
+                                             text_w_gibr + 20, text_h_gibr + 20), 2)
+        self.color_map = (0, 0, 0)
+
+        pygame.draw.rect(screen, (0, 0, 0), (601, 0, 120, 40), 2)
+        font = pygame.font.Font(None, 40)
+        text_find = font.render("Искать", 1, (0, 0, 0))
 
         running = True
 
-        search_line = InputWindow(0, 0, 600, 40, self.delta, self.tip, screen, self)
+        self.search_line = InputWindow(screen, self)
 
         map = request(self.coord, self.delta, self.tip)
 
@@ -135,6 +146,7 @@ class Window:
                 if event.type == pygame.QUIT:
                     running = False
                 if event.type == pygame.KEYDOWN:
+                    self.search_line.run_text = True
                     if event.key == 280:
                         if float(self.delta) + 2 < 17:
                             self.delta = str(float(self.delta) + 2)
@@ -148,36 +160,56 @@ class Window:
                     if event.key == pygame.K_UP:
                         if float(self.coord_y) + 1 < 80:
                             self.coord_y = str(float(self.coord_y) + 0.001)
+                            self.coord[1] = str(float(self.coord_y) + 0.001)
                             print(self.coord_y)
                             map = request(self.coord, self.delta, self.tip)
                     if event.key == pygame.K_DOWN:
                         if float(self.coord_y) - 1 > -80:
                             self.coord_y = str(float(self.coord_y) - 0.001)
+                            self.coord[1] = str(float(self.coord_y) - 0.001)
                             print(self.coord_y)
                             map = request(self.coord, self.delta, self.tip)
                     if event.key == pygame.K_LEFT:
                         if float(self.coord_x) - 1 > -180:
                             self.coord_x = str(float(self.coord_x) - 0.001)
+                            self.coord[0] = str(float(self.coord_x) - 0.001)
                             print(self.coord_x)
                             map = request(self.coord, self.delta, self.tip)
                     if event.key == pygame.K_RIGHT:
                         if float(self.coord_x) + 1 < 180:
                             self.coord_x = str(float(self.coord_x) + 0.001)
+                            self.coord[0] = str(float(self.coord_x) + 0.001)
                             print(self.coord_x)
                             map = request(self.coord, self.delta, self.tip)
-                    if event.key == pygame.K_1:
+                    # if event.key == pygame.K_1:
+                    if event.key in [pygame.K_1, pygame.KMOD_ALT]:
                         self.tip = "map"
                         map = request(self.coord, self.delta, self.tip)
-                    if event.key == pygame.K_2:
+                        self.search_line.run_text = False
+                    # if event.key == pygame.K_2:
+                    if event.key in [pygame.K_2, pygame.KMOD_ALT]:
                         self.tip = "sat"
                         map = request(self.coord, self.delta, self.tip)
-                    if event.key == pygame.K_3:
+                        self.search_line.run_text = False
+                    # if event.key == pygame.K_3:
+                    if event.key in [pygame.K_3, pygame.KMOD_ALT]:
                         self.tip = "sat,skl"
                         map = request(self.coord, self.delta, self.tip)
+                        self.search_line.run_text = False
                 if event.type == pygame.MOUSEBUTTONUP:
                     self.on_click(event.pos)
-                search_line.events(event)
-            search_line.draw(screen)
+                self.search_line.events(event)
+            self.search_line.draw(screen)
+            pygame.draw.rect(screen, self.color_map, (601, text_y_map - 10,
+                                                 630, text_h_map + 20), 2)
+            screen.blit(text_map, (text_x_map, text_y_map))
+            pygame.draw.rect(screen, self.color_spn, (601, text_y_spn - 10,
+                                                 text_w_spn + 20, text_h_spn + 20), 2)
+            screen.blit(text_spn, (text_x_spn, text_y_spn))
+            pygame.draw.rect(screen, self.color_gibr, (601, text_y_gibr - 10,
+                                                 text_w_gibr + 20, text_h_gibr + 20), 2)
+            screen.blit(text_gibr, (text_x_gibr, text_y_gibr))
+            screen.blit(text_find, (612, 8))
             screen.blit(pygame.image.load(map), (0, 40))
             pygame.display.flip()
         pygame.quit()
@@ -187,16 +219,19 @@ class Window:
     def on_click(self, cell_coords):
         # global delta, coord_x, coord_y, tip
         if cell_coords[0] > 600 and cell_coords[1] <= 40:
-            self.coord = InputWindow.search(InputWindow.text)
-            request(InputWindow.search(InputWindow.text), self.delta, self.tip)
+            self.coord = InputWindow.search(InputWindow, self.search_line.text)
+            request(InputWindow.search(InputWindow, self.search_line.text), self.delta, self.tip)
             self.text = ''
         if cell_coords[0] > 600 and 40 < cell_coords[1] <= 90:
+            self.color_map, self.color_spn, self.color_gibr = (0, 255, 0), (0, 0, 0), (0, 0, 0)
             self.tip = "map"
             map = request(self.coord, self.delta, self.tip)
         if cell_coords[0] > 600 and 90 < cell_coords[1] <= 140:
+            self.color_map, self.color_spn, self.color_gibr = (0, 0, 0), (0, 255, 0), (0, 0, 0)
             self.tip = "sat"
             map = request(self.coord, self.delta, self.tip)
         if cell_coords[0] > 600 and 140 < cell_coords[1] <= 190:
+            self.color_map, self.color_spn, self.color_gibr = (0, 0, 0), (0, 0, 0), (0, 255, 0)
             self.tip = "sat,skl"
             map = request(self.coord, self.delta, self.tip)
 
@@ -229,7 +264,4 @@ def request(coord, delta, tip):
     return map_file
 
 
-
-
-
-Window('37.490971', '55.829152', '0.02', "map")
+Window()
